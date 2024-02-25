cd29823b0af86e3f41319ad5e4e2166a968605db
Code Faisal <code.faisal.dev@gmail.com>
2024-02-26 00:38:00 +0600

    first commit

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,33 +1,30 @@
-import { graph, config, scalar } from "@grafbase/sdk";
+import { graph, config, auth } from '@grafbase/sdk';
 
-const g = graph.Standalone();
+const g = graph.Standalone()
 
-const User = g.type("User", {
-  name: scalar.string(),
-  email: scalar.email(),
-  avatarUrl: scalar.url(),
-  description: scalar.string(),
-  githubUrl: scalar.url(),
-  linkedinUrl: scalar.url(),
-  projects: g.ref("Project").optional().list(), // Use a string here instead of the Project type
-});
+// @ts-ignore
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(), 
+  projects: g.ref('Project').list().optional(),
+})
 
-const Project = g.type("Project", {
-  title: scalar.string(),
-  description: scalar.string(),
-  image: scalar.url(),
-  liveSiteUrl: scalar.url(),
-  category: scalar.string(),
+// @ts-ignore
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
+  category: g.string().search(),
   createdBy: g.ref(User),
-});
+})
+
 
 export default config({
   graph: g,
-
-  // auth: {
-
-  //   rules: (rules) => {
-  //     rules.public()
-  //   },
-  // },
-});
+})
